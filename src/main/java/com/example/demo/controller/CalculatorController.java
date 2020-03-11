ba3e80ceef469665104c3c8ed978eb3ff9196394
Semhal, Redda <rsemhal@gmail.com>
2020-03-10 23:00:44 -0400

    Added a controller class with add end point

--- src/main/java/com/example/demo/controller/CalculatorController.java
+++ src/main/java/com/example/demo/controller/CalculatorController.java
@@ -1,9 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.config.ClientConfig;
+import com.example.demo.service.Calculator;
 
 @RestController
 public class CalculatorController {
 	
+	AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ClientConfig.class);
+	Calculator calc = context.getBean(Calculator.class);
 	
+	
+	
+	@GetMapping({"/", "/add"})
+	public int add() {
+		return calc.add(7, 10);
+	}
 }
